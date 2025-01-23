b607b7ee766bef49657f0bd509d823d7daea0faf
Yoon Gun <dbsrjs0514@naver.com>
2025-01-23 19:59:11 +0900

    플레이어 이동 및 점프

--- Assets/02.Scripts/PlayerMove.cs
+++ Assets/02.Scripts/PlayerMove.cs
@@ -2,29 +2,43 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    private float moveSpeed = 3f;
-    private float jumpSpeed = 15f;
+    [Header("플레이어 이동")] 
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpPower;
+    private Vector2 moveInput;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private bool isGround = true;
     private Rigidbody rigid;
 
     private void Awake()
     {
+        UnityEngine.InputSystem.PlayerInput Input = GetComponent<UnityEngine.InputSystem.PlayerInput>();
         rigid = GetComponent<Rigidbody>();
+        moveAction = Input.actions["Move"];
+        //jumpAction = Input.actions["Jump"];
     }
 
     private void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        Vector3 moveVec = new Vector3(h, 0, v);
+        moveInput = moveAction.ReadValue<Vector2>();
+        rigid.velocity = new Vector3(moveInput.x * moveSpeed, rigid.velocity.y, moveInput.y * moveSpeed);
 
-        rigid.velocity = moveVec * moveSpeed;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        float rayLength = transform.localScale.y * 0.1f;
+        Debug.DrawRay(transform.position, Vector3.down * rayLength, Color.green);
+        isGround = Physics.Raycast(transform.position, Vector3.down, rayLength);
+        if (/*jumpAction.triggered*/ Input.GetKeyDown(KeyCode.Space) /*&& isGround*/)
         {
-            rigid.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            isGround = false;
         }
-}
+        
+    }
+    
+    
 }
