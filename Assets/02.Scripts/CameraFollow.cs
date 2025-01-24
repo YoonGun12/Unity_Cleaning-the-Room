a91c02f35695ffad6c8383903516a3c5b205ad02
Yoon Gun <dbsrjs0514@naver.com>
2025-01-24 21:30:48 +0900

    카메라 3인칭 이동, 회전 구현

--- Assets/02.Scripts/CameraFollow.cs
+++ Assets/02.Scripts/CameraFollow.cs
@@ -1,8 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [Header("카메라 추적 세팅")] 
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 cameraOffset;
+    [SerializeField] private float followSpeed;
+    [SerializeField] private float distanceTargetCamera = 0.1f;
+
+    [Header("카메라 회전 세팅")] 
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private float maxUpPitch = 70f;
+    [SerializeField] private float minDownPitch = -30f;
+
+    private float yaw; //y축 회전값(좌 우)
+    private float pitch; //x축 회전값(위 아래)
     
+
+    private void LateUpdate()
+    {
+        //마우스 입력
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        yaw += mouseX;
+        pitch -= mouseY;
+        pitch = Mathf.Clamp(pitch, minDownPitch, maxUpPitch);
+        
+        Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
+        Vector3 rotatedOffset = rotation * new Vector3(0, 0, -distanceTargetCamera);
+        transform.position = target.position + rotatedOffset;
+        
+        transform.LookAt(target.position + cameraOffset);
+    }
 }
--- Assets/02.Scripts/PlayerMove.cs
+++ Assets/02.Scripts/PlayerMove.cs
@@ -10,12 +10,22 @@ public class PlayerMove : MonoBehaviour
     [Header("플레이어 이동")] 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpPower;
+    [SerializeField] private Transform playerPivot;
+    [SerializeField] private float rotationSmoothTime = 0.1f;
+    
     private Vector2 moveInput;
     private InputAction moveAction;
     private InputAction jumpAction;
     private bool isGround;
     private Rigidbody rigid;
-    [SerializeField] private Transform playerPivot;
+    private float targetRotation;
+    private float rotationVelocity;
+
+    [Header("카메라")] 
+    [SerializeField] private Transform cameraTransform;
+
+    [Header("애니메이션")] 
+    private Animator anim;
 
     private void Awake()
     {
@@ -23,6 +33,7 @@ public class PlayerMove : MonoBehaviour
         rigid = GetComponent<Rigidbody>();
         moveAction = Input.actions["Move"];
         jumpAction = Input.actions["Jump"];
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -34,7 +45,27 @@ public class PlayerMove : MonoBehaviour
     private void Move()
     {
         moveInput = moveAction.ReadValue<Vector2>();
-        rigid.velocity = new Vector3(moveInput.x * moveSpeed, rigid.velocity.y, moveInput.y * moveSpeed);
+
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        //최종 이동 방향
+        Vector3 moveDirection = forward * moveInput.y + right * moveInput.x;
+
+        rigid.velocity = new Vector3(moveDirection.x * moveSpeed, rigid.velocity.y, moveDirection.z * moveSpeed);
+
+        if (moveDirection.sqrMagnitude > 0.01f)
+        {
+            targetRotation = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+            float smoothRotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity,
+                rotationSmoothTime);
+            transform.rotation = Quaternion.Euler(0, smoothRotation, 0);
+        }
     }
     
     private void Jump()
