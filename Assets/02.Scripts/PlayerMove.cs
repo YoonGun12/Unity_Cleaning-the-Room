9c6c6123b9c129e6c676f07022a88d6f26e5dd29
Yoon Gun <dbsrjs0514@naver.com>
2025-01-24 18:09:45 +0900

    InputSystem 점프 구현

--- Assets/02.Scripts/PlayerMove.cs
+++ Assets/02.Scripts/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,33 +13,40 @@ public class PlayerMove : MonoBehaviour
     private Vector2 moveInput;
     private InputAction moveAction;
     private InputAction jumpAction;
-    private bool isGround = true;
+    private bool isGround;
     private Rigidbody rigid;
+    [SerializeField] private Transform playerPivot;
 
     private void Awake()
     {
         UnityEngine.InputSystem.PlayerInput Input = GetComponent<UnityEngine.InputSystem.PlayerInput>();
         rigid = GetComponent<Rigidbody>();
         moveAction = Input.actions["Move"];
-        //jumpAction = Input.actions["Jump"];
+        jumpAction = Input.actions["Jump"];
     }
 
     private void Update()
     {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
         moveInput = moveAction.ReadValue<Vector2>();
         rigid.velocity = new Vector3(moveInput.x * moveSpeed, rigid.velocity.y, moveInput.y * moveSpeed);
-
-
-        float rayLength = transform.localScale.y * 0.1f;
-        Debug.DrawRay(transform.position, Vector3.down * rayLength, Color.green);
-        isGround = Physics.Raycast(transform.position, Vector3.down, rayLength);
-        if (/*jumpAction.triggered*/ Input.GetKeyDown(KeyCode.Space) /*&& isGround*/)
+    }
+    
+    private void Jump()
+    {
+        float rayLength = transform.localScale.y * 0.2f;
+        Debug.DrawRay(playerPivot.position, Vector3.down * rayLength, Color.green);
+        isGround = Physics.Raycast(playerPivot.position, Vector3.down, rayLength);
+        if (jumpAction.triggered && isGround)
         {
             rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
             isGround = false;
         }
-        
+
     }
-    
-    
 }
