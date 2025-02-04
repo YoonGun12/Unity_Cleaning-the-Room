3fd8edb7b38dee59fb5ff5e4f120be400ed12dfe
Yoon Gun <dbsrjs0514@naver.com>
2025-02-04 16:08:31 +0900

    GameManager.cs 중 UI로직들 분리
    UIManager.cs 생성
    GameManager.cs에서 UI관련 시간 감소, 파워게이지 로직 이동

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -1,72 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("능력게이지")]
-    [SerializeField] private Slider powerSlider;
-    [SerializeField] private float increasePowerSpeed;
-    [SerializeField] private float decreasePowerSpeed;
-    private bool isMousePressed;
-    
     [Header("게임 설정")] 
-    [SerializeField] private TextMeshProUGUI gameTimeText;
-    private float gameTime = 60f;
-
-    public int gameScore = 0;
+    [SerializeField] private UIManager uiManager; // UIManager 참조 추가
     
-    private void Update()
+    public int gameScore = 0;
+
+    private void Start()
     {
-        if (gameTime > 0)
+        if (uiManager != null)
         {
-            gameTime -= Time.deltaTime;
-            UpdateGameTime();
+            uiManager.SetGameTime(60f); // 초기 게임 시간 설정
         }
-        else
-        {
-            gameTime = 0;
-            gameTimeText.text = "00:00";
-            gameTimeText.color = Color.red;
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            isMousePressed = true;
-            DecreasePower();
-        }
-        else
-        {
-            isMousePressed = false;
-            IncreasePower();
-        }
-    }
-
-    private void UpdateGameTime()
-    {
-        int minutes = Mathf.FloorToInt(gameTime / 60);
-        int seconds = Mathf.FloorToInt(gameTime % 60);
-        gameTimeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-
-        if (gameTime <= 30f)
-        {
-            gameTimeText.color = Color.red;
-        }
-    }
-
-    private void IncreasePower()
-    {
-        if (!isMousePressed && powerSlider.value < powerSlider.maxValue)
-        {
-            powerSlider.value += increasePowerSpeed * Time.deltaTime;
-        }
-    }
-
-    private void DecreasePower()
-    {
-        powerSlider.value -= decreasePowerSpeed * Time.deltaTime;
     }
 }
